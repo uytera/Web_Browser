8ef16d097061a4cdf1ae5e3801b9c8bd28065fb2
uytera <uytera12@yandex.ru>
2019-10-17 07:44:41 +0500

    Email Check

--- src/main/java/com/filem/servlet/RegistrationServlet.java
+++ src/main/java/com/filem/servlet/RegistrationServlet.java
@@ -17,7 +17,7 @@ public class RegistrationServlet  extends HttpServlet {
         String pass = req.getParameter("pass");
         String email = req.getParameter("email");
         String stringFormat = "[a-zA-Z]+";
-        String emailFormat = "/.+@.+\\..+/i";
+        String emailFormat = ".+@.+\\..+";
         boolean s = email.matches(emailFormat);
         if(!login.matches(stringFormat) || !pass.matches(stringFormat)){
             resp.getWriter().println("Irritum format. Tessera et alias oportet continent solum latinis");
@@ -27,10 +27,10 @@ public class RegistrationServlet  extends HttpServlet {
             resp.getWriter().println("Alias et tessera, oportet quod sit amplius quam tres characteres");
             return;
         }
-        /*if(!email.matches(emailFormat)){
+        if(!email.matches(emailFormat)){
             resp.getWriter().println("Irritum mailing address");
             return;
-        }*/
+        }
         if(accountService.AddNewUser(login, pass, email)){
             resp.sendRedirect("/login/");
         }
